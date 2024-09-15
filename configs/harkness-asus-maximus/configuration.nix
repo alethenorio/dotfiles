0b5ab1ae02f0104349eb94f4e4990023a14dadcc
Alexandre Thenorio <alethenorio@gmail.com>
2024-09-15 16:04:56 +0200

    feat: add harkness configfeat: add harkness configss

---
+++ configs/harkness-asus-maximus/configuration.nix
@@ -0,0 +1,175 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../personal/configs/harkness-asus-maximus/configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "harkness"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Stockholm";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "sv_SE.UTF-8";
+    LC_IDENTIFICATION = "sv_SE.UTF-8";
+    LC_MEASUREMENT = "sv_SE.UTF-8";
+    LC_MONETARY = "sv_SE.UTF-8";
+    LC_NAME = "sv_SE.UTF-8";
+    LC_NUMERIC = "sv_SE.UTF-8";
+    LC_PAPER = "sv_SE.UTF-8";
+    LC_TELEPHONE = "sv_SE.UTF-8";
+    LC_TIME = "sv_SE.UTF-8";
+  };
+
+  # Configure keymap in X11
+  # services.xserver.xkb = {
+  #   layout = "se";
+  #   variant = "";
+  # };
+
+  # Configure console keymap
+  console.keyMap = "sv-latin1";
+
+  users.groups = {
+    tautulli = {};
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.mutableUsers = false;
+  users.users = {
+    alethenorio = {
+      createHome = true;
+      isNormalUser = true;
+      description = "Alexandre";
+      extraGroups = [ "networkmanager" "wheel" ];
+      hashedPassword = "$6$mZEeKCcpo$41ITgokQpFmMG5f6BQxS628Ne2YJ0aRqmpyyHXjrl9xgXFXc2XOyJlxs2zc5dCEtSXRBWLKHx0UEd4LmLGt0t.";
+      packages = with pkgs; [];
+    };
+    tautulli = {
+      isNormalUser = true;
+      group = "tautulli";
+    };
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "alethenorio";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    curl
+    file
+    gh
+    git
+    neovim
+    unzip
+  ];
+
+  environment.variables = {
+    EDITOR = "vim";
+  };
+
+  security.pam.loginLimits = [
+    { domain = "*"; item = "nofile"; type = "soft"; value = "99999"; }
+    { domain = "*"; item = "nofile"; type = "hard"; value = "99999"; }
+  ];
+
+  systemd.extraConfig = "DefaultLimitNOFILE=99999";
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Enable Plex
+  # In order to move the existing installation to a new server you will need to move data from
+  # the old server. See https://support.plex.tv/articles/201370363-move-an-install-to-another-system/
+  # for more details.
+  # When enabling Plex for the first time in a new machine, go to http://<machine_ip>:32400/web
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+    extraScanners = [
+      (pkgs.fetchFromGitHub {
+        owner = "ZeroQI";
+        repo = "Absolute-Series-Scanner";
+        rev = "4ef18a738c6263a8b96ab6f83ae391d4550b9cc9";
+        sha256 = "1lj8n77b7b3pxac4w82rcx8i60l3lxlkzm5jhkljy4apxv8nkdyr";
+      })
+    ];
+    extraPlugins = [
+      (pkgs.stdenv.mkDerivation{
+        name = "Hama.bundle";
+        src = pkgs.fetchFromGitHub {
+          owner = "ZeroQI";
+          repo = "Hama.bundle";
+          rev = "c24f2e336f7891807eacef816d270d973a151826";
+          sha256 = "0dlid5n6z4rvvxmvgmv70mdrh7wl0l38i3kxvzksmaj4xdqh3xkl";
+        };
+        buildPhase = ''
+          mkdir -p $out
+          cp -r . $out/
+        '';
+        dontInstall = true;
+      })
+      (pkgs.stdenv.mkDerivation{
+        name = "Traktv.bundle";
+        src = pkgs.fetchFromGitHub {
+          owner = "trakt";
+          repo = "Plex-Trakt-Scrobbler";
+          rev = "aeb0bfbe62fad4b06c164f1b95581da7f35dce0b";
+          sha256 = "058691ciy1c94icw91074im24a3gkc1rq9kz67cbzwb9h9ili70q";
+        };
+        buildPhase = ''
+         mkdir -p $out
+         cp -r ./Trakttv.bundle/* $out/
+        '';
+        dontInstall = true;
+      })
+    ];
+    #user = "plex";
+  };
+
+  # Enable Tautulli
+  services.tautulli = {
+    enable = true;
+    dataDir = "/var/lib/tautulli";
+    configFile = "/var/lib/tautulli/config.ini";
+    user = "tautulli";
+    group = "tautulli";
+  };
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [
+    # Tautulli
+    8181
+  ];
+
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
+
---
+++ configs/harkness-asus-maximus/home.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../../modules/home-manager/git
+    ../../modules/home-manager/neovim
+    ../../modules/home-manager/bash
+  ];
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "alethenorio";
+  home.homeDirectory = "/home/alethenorio";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  nixpkgs.config.allowUnfree = true;
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs;[
+    curl
+    file
+    (nerdfonts.override {fonts = ["SourceCodePro" "FiraCode" "Noto"];})
+    nethogs
+    nix-tree
+    speedtest-cli
+    udisks
+  ];
+
+  programs = {
+    gh = {
+      enable = true;
+    };
+    go = {
+      enable = true;
+      package = pkgs.go_1_21;
+      goPath = "go";
+      goBin = "go/bin";
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
+
