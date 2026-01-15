9bb445f75fcef2c5a9780586fe9ffeca9023e434
Alexandre Thenorio <2749771+alethenorio@users.noreply.github.com>
2026-01-15 18:19:55 +0100

    feat(neovim): start treesitter manually
    Previous changes fixed errors related to treesitter but it also more or
    less broke treesitter because recent changes done in the treesitter API
    remove functionality which would automatically enable syntax
    highlighting and other features.
    
    More detailed information can be found at
    https://github.com/nvim-treesitter/nvim-treesitters

--- modules/home-manager/neovim/lua/plugins/treesitter.lua
+++ modules/home-manager/neovim/lua/plugins/treesitter.lua
@@ -2,6 +2,19 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		--build = ":TSUpdate",
+		init = function()
+			-- Enable treesitter highlighting for all file types
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					local buf = vim.api.nvim_get_current_buf()
+					local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
+					-- Skip special buffers (prompts, terminals, popups, etc.)
+					if buftype == "" or buftype == "acwrite" then
+						vim.treesitter.start(buf)
+					end
+				end,
+			})
+		end,
 		opts = {
 			auto_install = false,
 			highlight = { enable = true },
