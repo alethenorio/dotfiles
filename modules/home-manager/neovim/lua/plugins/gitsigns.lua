86ed233206169e966bb6a3dfd8ccdf33095a60f0
Alexandre Thenorio <2749771+alethenorio@users.noreply.github.com>
2026-03-09 16:08:23 +0100

    feat(neovim): add gitsigns plugin configuration

---
+++ modules/home-manager/neovim/lua/plugins/gitsigns.lua
@@ -0,0 +1,26 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		signs = {
+			add = { text = "│" },
+			change = { text = "│" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+		},
+		on_attach = function(bufnr)
+			local gs = require("gitsigns")
+			local function map(mode, l, r, opts)
+				opts = opts or {}
+				opts.buffer = bufnr
+				vim.keymap.set(mode, l, r, opts)
+			end
+
+			map("n", "<leader>gb", function()
+				gs.blame_line({ full = true })
+			end, { desc = "[G]git [B]lame line" })
+			map("n", "<leader>gd", gs.diffthis, { desc = "[G]it [D]iff this" })
+		end,
+	},
+}
